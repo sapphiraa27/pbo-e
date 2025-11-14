e5b3f7c304891db06ea1bb4da39477e8dbda2810
Sapphira <sapphiraa237@gmail.com>
2025-11-14 13:19:36 +0700

    Update pertemuan 3

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,28 +1,15 @@
 <?php
 
 class LuasLingkaran {
+
     public const phi = 3.14;
     public int $jari;
-
-    public function __construct($isiJari = 1){
-        $this->jari = $isiJari;
-    }
-
-    public function tampil($nama) {
-        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "hasilnya {$nama} adalah: {$rumus}";
-    }
-
-    public static function testing() {
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-        echo "selesaaiii";
-    }
 }
 
 $lingkaran = new LuasLingkaran();
-$lingkaran->tampil('hijau'); //panggil method
+$lingkaran->jari = 12;
 
-LuasLingkaran::testing();
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+
+echo "Hasilnya adalah: " . $rumus;
+?>
